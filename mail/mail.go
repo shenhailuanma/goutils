261111fa32f41aea522f2f750a1dcf3e3da29938
shenhailuanma <zx051212@gmail.com>
2019-10-25 08:38:01 +0800

    mail

--- mail/mail.go
+++ mail/mail.go
@@ -1,25 +1,42 @@
 package mail
 
 import (
-	"fmt"
 	"gopkg.in/gomail.v2"
 )
 
-var MailUsername = ""
-var MailHost = ""
-var MailPort = 0
-var MailPassword = ""
+/**
+doc: https://tools.ietf.org/html/rfc2822.html
+*/
 
-func MailSendText(address string, subject string,  data string) error {
+type Mailer struct {
+	username string		// email sender's username
+	password string		// email sender's password
+	host     string		// email server host
+	port     int		// email server port
+}
+
+type MailInfo struct {
+	From    string
+	To      string
+	Subject string
+	Body    string
+}
+
+func NewMailer(username, password, host string, port int) (Mailer, error)  {
+	var mailer = Mailer{username:username, password:password, host:host, port:port}
+	return mailer, nil
+}
+
+
+func (mailer *Mailer)MailSendText(mailInfo MailInfo) error {
 
 	msg := gomail.NewMessage()
-	var from = fmt.Sprintf("Notifiction <%s>", MailUsername)
-	msg.SetHeader("From", from)
-	msg.SetHeader("To", address)
-	msg.SetHeader("Subject", subject)
-	msg.SetBody("text/plain", data)
+	msg.SetHeader("From", mailInfo.From)
+	msg.SetHeader("To", mailInfo.To)
+	msg.SetHeader("Subject", mailInfo.Subject)
+	msg.SetBody("text/plain", mailInfo.Body)
 
-	d := gomail.NewDialer(MailHost, MailPort,MailUsername, MailPassword)
+	d := gomail.NewDialer(mailer.host, mailer.port, mailer.username, mailer.password)
 	err := d.DialAndSend(msg)
 	if err != nil {
 		return err
@@ -27,18 +44,15 @@ func MailSendText(address string, subject string,  data string) error {
 	return nil
 }
 
-
-func MailSendHtml(address string, subject string,  data string) error {
+func (mailer *Mailer)MailSendHtml(mailInfo MailInfo) error {
 
 	msg := gomail.NewMessage()
+	msg.SetHeader("From", mailInfo.From)
+	msg.SetHeader("To", mailInfo.To)
+	msg.SetHeader("Subject", mailInfo.Subject)
+	msg.SetBody("text/html", mailInfo.Body)
 
-	var from = fmt.Sprintf("Notifiction <%s>", MailUsername)
-	msg.SetHeader("From", from)
-	msg.SetHeader("To", address)
-	msg.SetHeader("Subject", subject)
-	msg.SetBody("text/html", data)
-
-	d := gomail.NewDialer(MailHost, MailPort, MailUsername, MailPassword)
+	d := gomail.NewDialer(mailer.host, mailer.port, mailer.username, mailer.password)
 	err := d.DialAndSend(msg)
 	if err != nil {
 		return err
